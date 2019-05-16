aba14f4c960cfc11f3a037e143095d3007051443
confucianzuoyuan <zuoyuantc@126.com>
2019-05-16 23:49:06 +0800

    java to scala

---
+++ atguiguflink/src/main/scala/KafkaProducerUtil.scala
@@ -0,0 +1,23 @@
+import java.util.Properties
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+object KafkaProducerUtil {
+
+  def main(args: Array[String]): Unit = {
+    writeToKafka("hotitems")
+  }
+
+  def writeToKafka(topic: String): Unit = {
+    val props = new Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    val producer = new KafkaProducer[String, String](props)
+    val bufferedSource = io.Source.fromFile("/Users/yuanzuo/Desktop/flink-tutorial/atguiguflink/src/main/resources/UserBehavior.csv")
+    for (line <- bufferedSource.getLines) {
+      val record = new ProducerRecord[String, String](topic, line)
+      producer.send(record)
+    }
+    producer.close()
+  }
+}
