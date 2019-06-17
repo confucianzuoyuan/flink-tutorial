54543260ca251a6ecb3731ed532ff8a2561039de
confucianzuoyuan <zuoyuantc@126.com>
2019-06-17 23:57:46 +0800

    fix

--- FlinkEcommerce/src/main/scala/OrderTimeout.scala
+++ FlinkEcommerce/src/main/scala/OrderTimeout.scala
@@ -3,7 +3,7 @@ import org.apache.flink.cep.scala.pattern.Pattern
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.api.TimeCharacteristic
-
+import org.apache.flink.util.Collector
 import scala.collection.Map
 
 object OrderTimeout {
@@ -14,12 +14,23 @@ object OrderTimeout {
     env.setParallelism(1)
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
-    val orderEventStream = env.fromCollection(List(
-      OrderEvent("1", "create", "1558430842"),
-      OrderEvent("2", "create", "1558430843"),
-      OrderEvent("2", "pay", "1558430844"),
-      OrderEvent("3", "pay", "1558430942")
-    )).assignAscendingTimestamps(_.eventTime.toLong * 1000)
+//    val orderEventStream = env.fromCollection(List(
+//      OrderEvent("1", "create", "1558430842"),
+//      OrderEvent("2", "create", "1558430843"),
+//      OrderEvent("2", "pay", "1558430844"),
+//      OrderEvent("3", "pay", "1558430942")
+//    )).assignAscendingTimestamps(_.eventTime.toLong * 1000)
+
+    val orders: DataStream[String] = env.socketTextStream("localhost", 9999)
+
+    val orderEventStream = orders
+      .map(s => {
+        println(s)
+        val slist = s.split("\\|")
+        println(slist)
+        OrderEvent(slist(0), slist(1), slist(2))
+      })
+      .assignAscendingTimestamps(_.eventTime.toLong * 1000)
 
     val orderPayPattern = Pattern.begin[OrderEvent]("begin")
       .where(_.eventType.equals("create"))
@@ -27,30 +38,27 @@ object OrderTimeout {
       .where(_.eventType.equals("pay"))
       .within(Time.seconds(5))
 
-    val orderTimeoutOutput = OutputTag[OrderTimeoutEvent]("orderTimeout")
+    val orderTimeoutOutput = OutputTag[OrderEvent]("orderTimeout")
 
     val patternStream = CEP.pattern(orderEventStream.keyBy("orderId"), orderPayPattern)
 
-    val complexResult = patternStream.select(orderTimeoutOutput) {
-      (pattern: Map[String, Iterable[OrderEvent]], timestamp: Long) => {
-        println("timestamp: ", timestamp)
-        val createOrder = pattern.get("begin")
-        OrderTimeoutEvent(createOrder.get.iterator.next().orderId, "timeout")
-      }
-    } {
-      pattern: Map[String, Iterable[OrderEvent]] => {
-        val payOrder = pattern.get("next")
-        OrderTimeoutEvent(payOrder.get.iterator.next().orderId, "success")
-      }
+    val timeoutFunction = (map: Map[String, Iterable[OrderEvent]], timestamp: Long, out: Collector[OrderEvent]) => {
+      print(timestamp)
+      val orderStart = map.get("begin").get.head
+      out.collect(orderStart)
     }
-    val timeoutResult = complexResult.getSideOutput(orderTimeoutOutput)
 
-    complexResult.print()
-    timeoutResult.print()
+    val selectFunction = (map: Map[String, Iterable[OrderEvent]], out: Collector[OrderEvent]) => {
+    }
+
+    val timeoutOrder = patternStream.flatSelect(orderTimeoutOutput)(timeoutFunction)(selectFunction)
+
+    timeoutOrder.getSideOutput(orderTimeoutOutput).print()
 
     env.execute
 
   }
+
 
 }
 
