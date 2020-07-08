b3d3f3423c0e8f275d5e34ce22800ca9cbb43f18
confucianzuoyuan <zuoyuantc@126.com>
2020-07-08 14:12:31 +0800

    update

--- Flink0105/src/main/scala/com/atguigu/day2/KeyedStreamExample.scala
+++ Flink0105/src/main/scala/com/atguigu/day2/KeyedStreamExample.scala
@@ -16,10 +16,13 @@ object KeyedStreamExample {
 
     // 使用第三个字段来做滚动聚合，求每个传感器流上的最小温度值
     // 内部会保存一个最小值的状态变量，用来保存到来的温度的最小值
-    keyed.min(2).print()
+    // 滚动聚合以后，流的类型又变成了`DataStream`
+    val min : DataStream[SensorReading] = keyed.min(2)
+    min.print()
 
     // reduce也会保存一个状态变量
-    keyed.reduce((r1, r2) => SensorReading(r1.id, 0L, r1.temperature.min(r2.temperature))).print()
+    val red : DataStream[SensorReading] = keyed.reduce((r1, r2) => SensorReading(r1.id, 0L, r1.temperature.min(r2.temperature)))
+    red.print()
 
     env.execute()
   }
