066c23e3724326e9c3ab1c303127e12c915d88b7
confucianzuoyuan <zuoyuantc@126.com>
2020-07-10 16:17:09 +0800

    update

--- Flink0105/src/main/scala/com/atguigu/day3/GenWatermark.scala
+++ Flink0105/src/main/scala/com/atguigu/day3/GenWatermark.scala
@@ -3,9 +3,11 @@ package com.atguigu.day3
 import java.sql.Timestamp
 
 import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.scala.function.ProcessWindowFunction
+import org.apache.flink.streaming.api.watermark.Watermark
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.util.Collector
@@ -19,7 +21,7 @@ import org.apache.flink.util.Collector
 // a 15
 // a 1
 // a 1
-object EventTimeExample {
+object GenWatermark {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
@@ -27,6 +29,9 @@ object EventTimeExample {
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
     // 系统默认每隔200ms插入一次水位线
+    // 设置为每隔一分钟插入一次水位线
+    env.getConfig.setAutoWatermarkInterval(60000)
+
 
     val stream = env
       .socketTextStream("localhost", 9999, '\n')
@@ -38,11 +43,7 @@ object EventTimeExample {
       // 分配时间戳和水位线一定要在keyBy之前进行！
       // 水位线 = 系统观察到的元素携带的最大时间戳 - 最大延迟时间
       .assignTimestampsAndWatermarks(
-        // 设置事件的最大延迟时间是5s
-        new BoundedOutOfOrdernessTimestampExtractor[(String, Long)](Time.seconds(5)) {
-          // 告诉系统，时间戳是元组的第二个字段
-          override def extractTimestamp(element: (String, Long)): Long = element._2
-        }
+        new MyAssigner
       )
       .keyBy(_._1)
       .timeWindow(Time.seconds(10))
@@ -50,6 +51,29 @@ object EventTimeExample {
 
     stream.print()
     env.execute()
+  }
+
+  // 周期性的插入水位线
+  class MyAssigner extends AssignerWithPeriodicWatermarks[(String, Long)] {
+    // 设置最大延迟时间
+    val bound: Long = 10 * 1000L
+    // 系统观察到的元素包含的最大时间戳
+    var maxTs: Long = Long.MinValue + bound
+
+    // 定义抽取时间戳的逻辑，每到一个事件就调用一次
+    override def extractTimestamp(element: (String, Long), previousElementTimestamp: Long): Long = {
+      maxTs = maxTs.max(element._2) // 更新观察到的最大时间戳
+      element._2 // 将抽取的时间戳返回
+    }
+
+    // 产生水位线的逻辑
+    // 默认每隔200ms调用一次
+    // 我们设置了每隔1分钟调用一次
+    override def getCurrentWatermark: Watermark = {
+      // 观察到的最大事件时间 - 最大延迟时间
+      println("观察到的最大时间戳是：" + maxTs)
+      new Watermark(maxTs - bound)
+    }
   }
 
   class WindowResult extends ProcessWindowFunction[(String, Long), String, String, TimeWindow] {
