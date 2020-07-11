1a94a7521b1f1321899a970b81428bc9c6bf9570
confucianzuoyuan <zuoyuantc@126.com>
2020-07-11 15:49:26 +0800

    test

---
+++ FlinkSZ1128/src/main/scala/com/atguigu/day4/TestWatermark.scala
@@ -0,0 +1,43 @@
+package com.atguigu.day4
+
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.util.Collector
+
+object TestWatermark {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+    env.setParallelism(1)
+
+    val stream1 = env
+      .socketTextStream("localhost", 9999, '\n')
+      .map(line => {
+        val arr = line.split(" ")
+        (arr(0), arr(1).toLong * 1000)
+      })
+      .assignAscendingTimestamps(_._2)
+
+    //a 1
+    //a 2
+    //a 3
+    val stream2 = env
+      .socketTextStream("localhost", 9998, '\n')
+      .map(line => {
+        val arr = line.split(" ")
+        (arr(0), arr(1).toLong * 1000)
+      })
+      .assignAscendingTimestamps(_._2)
+
+    stream1.union(stream2).keyBy(_._1).process(new Keyed).print()
+
+    env.execute()
+  }
+
+  class Keyed extends KeyedProcessFunction[String, (String, Long), String] {
+    override def processElement(value: (String, Long), ctx: KeyedProcessFunction[String, (String, Long), String]#Context, out: Collector[String]): Unit = {
+      println("watermark is " + ctx.timerService().currentWatermark())
+    }
+  }
+}
