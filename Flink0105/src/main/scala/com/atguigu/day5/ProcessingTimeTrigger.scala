f147532558b06d7dd3f28a679bf81762ec9063e8
confucianzuoyuan <zuoyuantc@126.com>
2020-07-14 10:56:55 +0800

    update

--- Flink0105/src/main/scala/com/atguigu/day5/ProcessingTimeTrigger.scala
+++ Flink0105/src/main/scala/com/atguigu/day5/ProcessingTimeTrigger.scala
@@ -1,8 +1,8 @@
 package com.atguigu.day5
 
+import com.atguigu.day2.{SensorReading, SensorSource}
 import org.apache.flink.api.common.state.ValueStateDescriptor
 import org.apache.flink.api.scala.typeutils.Types
-import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.scala.function.ProcessWindowFunction
 import org.apache.flink.streaming.api.windowing.time.Time
@@ -11,20 +11,15 @@ import org.apache.flink.streaming.api.windowing.triggers.{Trigger, TriggerResult
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.util.Collector
 
-object TriggerExample {
+object ProcessingTimeTrigger {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
-    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
     env.setParallelism(1)
 
     val stream = env
-      .socketTextStream("localhost", 9999, '\n')
-      .map(line => {
-        val arr = line.split(" ")
-        (arr(0), arr(1).toLong)
-      })
-      .assignAscendingTimestamps(_._2)
-      .keyBy(_._1)
+      .addSource(new SensorSource)
+      .filter(r => r.id.equals("sensor_1"))
+      .keyBy(_.id)
       .timeWindow(Time.seconds(10))
       .trigger(new OneSecondIntervalTrigger)
       .process(new WindowCount)
@@ -35,55 +30,45 @@ object TriggerExample {
   }
 
   // 只在整数秒和窗口结束时间时触发窗口计算！
-  class OneSecondIntervalTrigger extends Trigger[(String, Long), TimeWindow] {
+  class OneSecondIntervalTrigger extends Trigger[SensorReading, TimeWindow] {
     // 每来一条数据都要调用一次！
-    override def onElement(element: (String, Long), timestamp: Long, window: TimeWindow, ctx: TriggerContext): TriggerResult = {
+    override def onElement(element: SensorReading, timestamp: Long, window: TimeWindow, ctx: TriggerContext): TriggerResult = {
       // 默认值为false
       // 当第一条事件来的时候，会在后面的代码中将firstSeen置为true
       val firstSeen = ctx.getPartitionedState(
         new ValueStateDescriptor[Boolean]("first-seen", Types.of[Boolean])
       )
 
-      // 当第一条数据来的时候，!firstSeen.value()为true
-      // 仅对第一条数据注册定时器
-      // 这里的定时器指的是：onEventTime函数！
       if (!firstSeen.value()) {
-        // 第一条数据`a 1234`来的时候，水位线是：-9223372036854775808
-        // 过了200ms，插入了一个水位线1234 - 1 = 1233
-        // 水位线后面的整数秒是：-9223372036854774000
-        println("第一条数据来了！当前水位线是：" + ctx.getCurrentWatermark)
-        val t = ctx.getCurrentWatermark + (1000 - (ctx.getCurrentWatermark % 1000))
-        println("第一条数据来了以后，注册的定时器的整数秒的时间戳是：" + t)
-        ctx.registerEventTimeTimer(t) // 在第一条数据的时间戳之后的整数秒注册一个定时器
-        ctx.registerEventTimeTimer(window.getEnd) // 在窗口结束事件注册一个定时器
+        val t = ctx.getCurrentProcessingTime + (1000 - (ctx.getCurrentProcessingTime % 1000))
+        ctx.registerProcessingTimeTimer(t) // 在第一条数据的时间戳之后的整数秒注册一个定时器
+        ctx.registerProcessingTimeTimer(window.getEnd) // 在窗口结束事件注册一个定时器
         firstSeen.update(true)
       }
       TriggerResult.CONTINUE
     }
 
     override def onProcessingTime(time: Long, window: TimeWindow, ctx: TriggerContext): TriggerResult = {
-      TriggerResult.CONTINUE
-    }
-
-    // 定时器函数，在水位线到达time时，触发
-    override def onEventTime(time: Long, window: TimeWindow, ctx: TriggerContext): TriggerResult = {
       // 在onElement函数中，我们注册过窗口结束时间的定时器
+      // 将窗口闭合的默认触发操作override掉了。
       if (time == window.getEnd) {
         // 在窗口闭合时，触发计算并清空窗口
         TriggerResult.FIRE_AND_PURGE
       } else {
         // 1233ms后面的整数秒是2000ms
-        val t = ctx.getCurrentWatermark + (1000 - (ctx.getCurrentWatermark % 1000))
+        val t = ctx.getCurrentProcessingTime + (1000 - (ctx.getCurrentProcessingTime % 1000))
         // 保证t小于窗口结束时间
         if (t < window.getEnd) {
-          println("注册的定时器的整数秒的时间戳是：" + t)
-          // 这里注册的定时器还是onEventTime函数
-          ctx.registerEventTimeTimer(t)
+          ctx.registerProcessingTimeTimer(t)
         }
-        // 触发窗口计算
-        println("在 " + time + " 触发了窗口计算！")
+        println("定时器触发的时间为：" + time)
         TriggerResult.FIRE
       }
+    }
+
+    // 定时器函数，在水位线到达time时，触发
+    override def onEventTime(time: Long, window: TimeWindow, ctx: TriggerContext): TriggerResult = {
+      TriggerResult.CONTINUE
     }
 
     override def clear(window: TimeWindow, ctx: TriggerContext): Unit = {
@@ -96,8 +81,8 @@ object TriggerExample {
     }
   }
 
-  class WindowCount extends ProcessWindowFunction[(String, Long), String, String, TimeWindow] {
-    override def process(key: String, context: Context, elements: Iterable[(String, Long)], out: Collector[String]): Unit = {
+  class WindowCount extends ProcessWindowFunction[SensorReading, String, String, TimeWindow] {
+    override def process(key: String, context: Context, elements: Iterable[SensorReading], out: Collector[String]): Unit = {
       out.collect("窗口中有 " + elements.size + " 条数据！窗口结束时间是" + context.window.getEnd)
     }
   }
