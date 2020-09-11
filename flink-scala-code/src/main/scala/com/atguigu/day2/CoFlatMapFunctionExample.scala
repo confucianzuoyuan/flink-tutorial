22c0167e3621ece56ba5732033014016934c4001
confucianzuoyuan <zuoyuantc@126.com>
2020-09-11 16:14:47 +0800

    update

--- flink-scala-code/src/main/scala/com/atguigu/day2/CoFlatMapFunctionExample.scala
+++ flink-scala-code/src/main/scala/com/atguigu/day2/CoFlatMapFunctionExample.scala
@@ -9,21 +9,23 @@ object CoFlatMapFunctionExample {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
 
-    val stream1: KeyedStream[(Int, String), Int] = env
+    val stream1 = env
       .fromElements(
         (1, "aaaaa"),
         (2, "bbbbb")
       )
-      .keyBy(r => r._1)
 
-    val stream2: KeyedStream[(Int, String), Int] = env
+    val stream2 = env
       .fromElements(
         (1, "ccccc"),
         (2, "ddddd")
       )
-      .keyBy(r => r._1)
 
-    val connected: ConnectedStreams[(Int, String), (Int, String)] = stream1.connect(stream2)
+    val connected: ConnectedStreams[(Int, String), (Int, String)] = stream1
+      .keyBy(r => r._1)
+      .connect(stream2.keyBy(r => r._1))
+
+    val connected1 = stream1.connect(stream2).keyBy(0, 0)
 
     connected
       .flatMap(new MyFlatMapFunction)
@@ -34,25 +36,13 @@ object CoFlatMapFunctionExample {
 
   class MyFlatMapFunction extends CoFlatMapFunction[(Int, String), (Int, String), String] {
 
-    private var v: String = _
-
     override def flatMap1(value: (Int, String), out: Collector[String]): Unit = {
       out.collect(value._2 + " 来自第一条流的元素发送两次")
       out.collect(value._2 + " 来自第一条流的元素发送两次")
-      if (v != null) {
-        out.collect(v + value._2)
-      } else {
-        v = value._2
-      }
     }
 
     override def flatMap2(value: (Int, String), out: Collector[String]): Unit = {
       out.collect(value._2 + " 来自第二条流的元素发送一次")
-      if (v != null) {
-        out.collect(v + value._2)
-      } else {
-        v = value._2
-      }
     }
   }
 }
