35be966e17e34cd6e70bc8204c17d5bd0af14c56
confucianzuoyuan <zuoyuantc@126.com>
2020-09-30 10:40:55 +0800

    update

--- docs/src/chapter06-05-03-使用迟到元素更新窗口计算结果.md
+++ docs/src/chapter06-05-03-使用迟到元素更新窗口计算结果.md
@@ -21,14 +21,13 @@ val countPer10Secs: DataStream[(String, Long, Int, String)] = readings
   // count readings and update results if late readings arrive
   .process(new UpdatingWindowCountFunction)
 
-  /** A counting WindowProcessFunction that distinguishes between 
+  /** A counting WindowProcessFunction that distinguishes between
   * first results and updates. */
 class UpdatingWindowCountFunction
     extends ProcessWindowFunction[SensorReading,
       (String, Long, Int, String), String, TimeWindow] {
 
-  @Override
-public process(
+  override def process(
       id: String,
       ctx: Context,
       elements: Iterable[SensorReading],
@@ -56,3 +55,65 @@ public process(
 }
 ```
 
+**java version**
+
+```java
+public class UpdateWindowResultWithLateEvent {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        DataStreamSource<String> stream = env.socketTextStream("localhost", 9999);
+
+        stream
+                .map(new MapFunction<String, Tuple2<String, Long>>() {
+                    @Override
+                    public Tuple2<String, Long> map(String s) throws Exception {
+                        String[] arr = s.split(" ");
+                        return Tuple2.of(arr[0], Long.parseLong(arr[1]) * 1000L);
+                    }
+                })
+                .assignTimestampsAndWatermarks(
+                        WatermarkStrategy.<Tuple2<String, Long>>forBoundedOutOfOrderness(Duration.ofSeconds(5))
+                        .withTimestampAssigner(new SerializableTimestampAssigner<Tuple2<String, Long>>() {
+                            @Override
+                            public long extractTimestamp(Tuple2<String, Long> stringLongTuple2, long l) {
+                                return stringLongTuple2.f1;
+                            }
+                        })
+                )
+                .keyBy(r -> r.f0)
+                .timeWindow(Time.seconds(5))
+                .allowedLateness(Time.seconds(5))
+                .process(new UpdateWindowResult())
+                .print();
+
+        env.execute();
+    }
+
+    public static class UpdateWindowResult extends ProcessWindowFunction<Tuple2<String, Long>, String, String, TimeWindow> {
+        @Override
+        public void process(String s, Context context, Iterable<Tuple2<String, Long>> iterable, Collector<String> collector) throws Exception {
+            long count = 0L;
+            for (Tuple2<String, Long> i : iterable) {
+                count += 1;
+            }
+
+            // 可见范围比getRuntimeContext.getState更小，只对当前key、当前window可见
+            // 基于窗口的状态变量，只能当前key和当前窗口访问
+            ValueState<Boolean> isUpdate = context.windowState().getState(
+                    new ValueStateDescriptor<Boolean>("isUpdate", Types.BOOLEAN)
+            );
+
+            // 当水位线超过窗口结束时间时，触发窗口的第一次计算！
+            if (isUpdate.value() == null) {
+                collector.collect("窗口第一次触发计算！一共有 " + count + " 条数据！");
+                isUpdate.update(true);
+            } else {
+                collector.collect("窗口更新了！一共有 " + count + " 条数据！");
+            }
+        }
+    }
+}
+```
---
+++ flink-code-java-0421/src/main/java/com/atguigu/day05/TwoWIndowJoin.java
@@ -0,0 +1,72 @@
+package com.atguigu.day05;
+
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.JoinFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+public class TwoWIndowJoin {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+        SingleOutputStreamOperator<Tuple2<String, Long>> stream1 = env
+                .fromElements(
+                        Tuple2.of("a", 1000L),
+                        Tuple2.of("b", 1000L),
+                        Tuple2.of("a", 2000L),
+                        Tuple2.of("b", 2000L)
+                )
+                .assignTimestampsAndWatermarks(
+                        WatermarkStrategy.<Tuple2<String, Long>>forMonotonousTimestamps()
+                                .withTimestampAssigner(
+                                        new SerializableTimestampAssigner<Tuple2<String, Long>>() {
+                                            @Override
+                                            public long extractTimestamp(Tuple2<String, Long> r, long l) {
+                                                return r.f1;
+                                            }
+                                        }
+                                )
+                );
+
+        SingleOutputStreamOperator<Tuple2<String, Long>> stream2 = env
+                .fromElements(
+                        Tuple2.of("a", 3000L),
+                        Tuple2.of("b", 3000L),
+                        Tuple2.of("a", 4000L),
+                        Tuple2.of("b", 4000L)
+                )
+                .assignTimestampsAndWatermarks(
+                        WatermarkStrategy.<Tuple2<String, Long>>forMonotonousTimestamps()
+                                .withTimestampAssigner(
+                                        new SerializableTimestampAssigner<Tuple2<String, Long>>() {
+                                            @Override
+                                            public long extractTimestamp(Tuple2<String, Long> r, long l) {
+                                                return r.f1;
+                                            }
+                                        }
+                                )
+                );
+
+        stream1
+                .join(stream2)
+                .where(r -> r.f0)
+                .equalTo(r -> r.f0) // on stream1.f0 = stream2.f0
+                .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+                .apply(new JoinFunction<Tuple2<String, Long>, Tuple2<String, Long>, String>() {
+                    @Override
+                    public String join(Tuple2<String, Long> r1, Tuple2<String, Long> r2) throws Exception {
+                        return r1 + " => " + r2;
+                    }
+                })
+                .print();
+
+        env.execute();
+    }
+}
